e4304137c9c97269229c741746f17d7da2701d85
Marcelo <desenvolvimento2@bcseventos.com.br>
2024-02-27 10:38:30 -0300

    index

---
+++ app.js
@@ -0,0 +1,12 @@
+//console.log(document.getElementById("app"));
+
+document.getElementById("main").innerHTML+= '<h1>Hello World</h1>';
+document.getElementById("main").innerHTML+= '<h2>Hello World</h2>';
+document.getElementById("main").innerHTML+= '<h3>Hello World</h3>';
+document.getElementById("main").innerHTML+= '<p>Hello World</p>';
+document.getElementById("main").innerHTML+= '<p class="p-red">Hello World</p>';
+document.getElementById("main").innerHTML+= '<p class="p-blue">Hello World</p>';
+document.getElementById("main").innerHTML+= '<p class="p-green">Hello World</p>';
+document.getElementById("main").innerHTML+= '<p class="p-dark p-bold">Hello World</p>';
+document.getElementById("main").innerHTML+= '<p class="p-dark p-italic">Hello World</p>';
+document.getElementById("main").innerHTML+= '<p class="p-dark p-underline">Hello World</p>';
